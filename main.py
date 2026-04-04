4d0b16e0277fdea1a96285d41f17b1dbce6ea081
M Aliyan <feldsheroffical@gmail.com>
2026-04-05 00:24:42 +0500

    Fix: Update CORS to whitelist netlify domain

--- main.py
+++ main.py
@@ -79,7 +79,11 @@ async def shutdown_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://akdropservicing.netlify.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
